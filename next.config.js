8f7cac7e479c6e9093bb9eb9e9305bb7a15fe03f
Adamovproga2003 <gutrfknc@gmail.com>
2023-07-04 00:53:27 +0300

    For server

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-	output: "export",
-	reactStrictMode: true,
-	// Add basePath
-	basePath: "/faculty-project",
+	// output: "export",
+	// reactStrictMode: true,
+	// // Add basePath
+	// basePath: "/faculty-project",
 	// Optional: Add a trailing slash to all paths `/about` -> `/about/`
 	// trailingSlash: true,
 	// Optional: Change the output directory `out` -> `dist`
